0bcbb3e5a7abbb12e3c671e578eb05e9c384d13d
Robby <fzpinus@gmail.com>
2017-08-31 09:14:15 +0800

    feat(289): Game of Life

---
+++ 289_gameOfLife.py
@@ -0,0 +1,37 @@
+# According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+# Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+# Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+# Any live cell with two or three live neighbors lives on to the next generation.
+# Any live cell with more than three live neighbors dies, as if by over-population..
+# Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+# Write a function to compute the next state (after one update) of the board given its current state.
+
+# Follow up: 
+# Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
+# In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
+class Solution(object):
+  def gameOfLife(self, board):
+    """
+    :type board: List[List[int]]
+    :rtype: void Do not return anything, modify board in-place instead.
+    """
+    if not board or not board[0]:
+      return
+    m, n = len(board), len(board[0])
+    for i in xrange(m):
+      for j in xrange(n):
+        self.update(board, m, n, i, j)
+    for i in xrange(m):
+      for j in xrange(n):
+        board[i][j] >>= 1
+
+  def update(self, board, m, n, i, j):
+    live = 0
+    for p in xrange(max(i - 1, 0), min(i + 2, m)):
+      for q in xrange(max(j - 1, 0), min(j + 2, n)):
+        live += board[p][q] & 1
+    if live === 3 or live == board[i][j] + 3:
+      board[i][j] += 2
+
