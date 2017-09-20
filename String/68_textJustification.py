12445e02b233be0b63d8d7fe33b7f5894cef0a87
Robby <fzpinus@gmail.com>
2017-09-19 18:02:44 -0700

    feat(125): Valid Palindrome

---
+++ String/68_textJustification.py
@@ -0,0 +1,12 @@
+class Solution(self, s):
+  def fullJustify(self, words, maxWidth):
+    res, curr, num_of_letters = [], [], 0
+    for w in words:
+      if num_of_letters + len(w) + len(curr) > maxWidth:
+        for i in range(maxWidth - num_of_letters):
+          curr[i % (len(curr) - 1 or 1)] += ' '
+        res.append(''.join(curr))
+        curr, num_of_letters = [], 0
+      curr += [w]
+      num_of_letters += len(w)
+    return res + [' '.join(curr).ljust(maxWidth)]
